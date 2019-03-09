02d139fcc84dc40963c6515bffacc34bfbd73f82
Adam-Koza <adam.koza@mail.utoronto.ca>
2019-03-09 12:09:56 -0500

    Fix port issue.

--- q1.js
+++ q1.js
@@ -14,5 +14,5 @@ server.post('/messageParrot', (request, response) => {
 
 
 server.listen(process.env.PORT || 3000, function () {
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+    console.log("Express server listening on port %d in %s mode", this.address().port, server.settings.env);
 });
