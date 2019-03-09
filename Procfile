03b2cadf4eb83c38a3e1ce8cdf342b1ffe0781ac
Adam-Koza <adam.koza@mail.utoronto.ca>
2019-03-09 10:35:07 -0500

    Add server and Procfile.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node q1.js
---
+++ q1.js
@@ -0,0 +1,16 @@
+const express     = require('express');
+const bodyParser  = require("body-parser");
+const server      = express();
+
+server.use(bodyParser.json());
+
+server.set('port', process.env.PORT || 3000);
+
+server.post('/messageParrot', (request, response) => {
+    let resString = "Message is: " + request.body.message + ", Detail is: " + request.body.detail;
+        response.send(resString);
+
+});
+
+
+server.listen(3000, () => { console.log('Node server created at port 3000') });
