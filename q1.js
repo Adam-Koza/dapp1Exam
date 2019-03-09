99fc5c560592ccc3ebf9f5eef64b38a1fd082c86
Adam-Koza <adam.koza@mail.utoronto.ca>
2019-03-09 12:08:26 -0500

    Fix port issue.

--- q1.js
+++ q1.js
@@ -1,20 +1,18 @@
-const express     = require('express');
-const bodyParser  = require("body-parser");
-const server      = express();
+const express = require('express');
+const bodyParser = require("body-parser");
+const server = express();
 
 server.use(bodyParser.json());
 
 server.set('port', process.env.PORT || 8080);
 
-//server.get('/', (request, response) => {
-//    response.send('Welcome to Dapp1 Exam');
-//});
-
 server.post('/messageParrot', (request, response) => {
     let resString = "Message is: " + request.body.message + ", Detail is: " + request.body.detail;
-        response.send(resString);
+    response.send(resString);
 
 });
 
 
-server.listen(8080, () => { console.log('Node server created at port 8080') });
+server.listen(process.env.PORT || 3000, function () {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
