b2fe1730333699ce160b2cf0a8245b218afee0b7
Adam-Koza <adam.koza@mail.utoronto.ca>
2019-03-09 10:45:41 -0500

    Add root route.

--- q1.js
+++ q1.js
@@ -6,6 +6,10 @@ server.use(bodyParser.json());
 
 server.set('port', process.env.PORT || 3000);
 
+server.get('/', (request, response) => {
+    response.send('Welcome to Dapp1 Exam');
+});
+
 server.post('/messageParrot', (request, response) => {
     let resString = "Message is: " + request.body.message + ", Detail is: " + request.body.detail;
         response.send(resString);
